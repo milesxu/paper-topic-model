3ad0e8e59a82f0ffdf0161f2b94d076e844c1664
milesxu <milesxu@outlook.com>
2019-01-24 19:35:54 +0800

    add mathjax

--- static/paper-app/src/app/paper-detail/paper-detail.component.ts
+++ static/paper-app/src/app/paper-detail/paper-detail.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ElementRef,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { Paper } from '../paper';
+declare var MathJax: any;
 
 @Component({
   selector: 'app-paper-detail',
   templateUrl: './paper-detail.component.html',
   styleUrls: ['./paper-detail.component.css']
 })
-export class PaperDetailComponent implements OnInit {
+export class PaperDetailComponent implements OnInit, AfterViewInit {
   @Input() paper: Paper;
+  @ViewChild('equantions') equations: ElementRef;
   constructor() {}
 
-  ngOnInit() {}
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    /*this.equations.nativeElement.innerHTML = this.paper.abstract;
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.equations.nativeElement]);*/
+    const element = document.getElementsByClassName('abstract');
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, element]);
+  }
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+  }
 }
